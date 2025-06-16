c05600721bc7aa7824248bd62ca4e7cc60db2e0d
Michael Gur <michael@thegur.net>
2025-06-16 18:27:06 +0300

    expose steps

--- out/main.js
+++ out/main.js
@@ -707,7 +707,6 @@
       // find steps
       const stepElements = Array.from(form.querySelectorAll("fieldset"));
       /** @type {Array<StepManager>} */
-      /*@__MANGLE_PROP__*/
       this.steps = stepElements.map((el) => new StepManager(this, el));
 
       // bind UI
