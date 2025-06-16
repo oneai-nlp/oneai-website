2b00f38cf77d2db90314ecca0ba9033463fa7ef8
Michael Gur <michael@thegur.net>
2025-06-16 19:11:46 +0300

    added analytics

--- out/main.js
+++ out/main.js
@@ -1,5 +1,257 @@
 (function () {
 
+  function isMobile$1() {
+    var check = false;
+    (function(a) {
+      if (
+        /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+          a,
+        ) ||
+        /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+          a.substr(0, 4),
+        )
+      )
+        check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+  }
+
+  function uuid() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+      var r = (Math.random() * 16) | 0,
+        v = c === "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+
+  const urlParams$1 = (function() {
+    const urlParams = {};
+    const querystring = window.location.toString().split("#")[0].split("?");
+    if (querystring.length > 1) {
+      const pairs = querystring[1].split("&");
+      for (const pair of pairs) {
+        const keyval = pair.split("=");
+        urlParams[keyval[0]] = decodeURIComponent(keyval[1]);
+      }
+    }
+    return urlParams;
+  })();
+
+  function getCookie$1(name) {
+    const value = ` ; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length >= 2) return parts[1].split(";").shift();
+  }
+
+  // true on success, otherwise false
+  function setCookie(name, value, days = 0) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie =
+      name +
+      "=" +
+      (value || "") +
+      expires +
+      `;domain=.${window.location.hostname};path=/`;
+    // not a hard ===, as we convert value to string
+    return getCookie$1(name) == value;
+  }
+
+  // return cookie if exists, otherwise try to create it, return true on success, otherwise false
+  function getOrSetCookie(name, value, days = 0) {
+    return getCookie$1(name) || setCookie(name, value, days);
+  }
+
+  // only true if setCookie was called and ran successfully
+  const visitorCreated =
+    (getCookie$1("ONEAI_UNIQUE_ID") || setCookie("ONEAI_UNIQUE_ID", uuid())) ===
+    true;
+
+  function triggerEvent(data) {
+    console.debug("triggering event:", isMobile$1() ? JSON.stringify(data) : data);
+    const { type, value, ...d } = data;
+    return fetch("https://api.oneai.com/analytics/web", {
+      keepalive: true,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type,
+        value,
+        date_time: new Date().toISOString().split(".")[0],
+        device: isMobile$1() ? "Mobile" : "Desktop",
+        one_id: getCookie$1("ONEAI_UNIQUE_ID"),
+        path: window.location.pathname,
+        version: window.version,
+        data: {
+          ...d,
+          os: navigator?.userAgentData?.platform || "",
+          user_agent: navigator?.userAgent || "",
+          referrer: document.referrer,
+          utm_source: urlParams$1["utm_source"] || "",
+          utm_medium: urlParams$1["utm_medium"] || "",
+          utm_campaign: urlParams$1["utm_campaign"] || "",
+          utm_term: urlParams$1["utm_term"] || "",
+          groupid: urlParams$1["groupid"] || "",
+          campaignid: urlParams$1["campaignid"] || "",
+        },
+      }),
+    });
+  }
+
+  function triggerAgentEvent(data) {
+    fetch("https://api.oneai.com/analytics/agent", {
+      keepalive: true,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        agent_name: "",
+        agent_id: "",
+        chat_id: "",
+        is_owner: true,
+        host_domain: "oneai.com",
+        host_path: window.location.pathname,
+        org_id: "",
+        user_id: "",
+        user_email: "",
+        user_status: "anonymous",
+        api_key: "ANONYMOUS",
+        session_id: "",
+        datetime: new Date().toISOString().split(".")[0],
+        timestamp: new Date().toISOString().split(".")[0],
+        one_id: getCookie$1("ONEAI_UNIQUE_ID"),
+        path: window.location.pathname,
+        ...data,
+        data: {
+          utm: {
+            utm_content: urlParams$1["utm_content"] || "",
+            utm_source: urlParams$1["utm_source"] || "",
+            utm_medium: urlParams$1["utm_medium"] || "",
+            utm_campaign: urlParams$1["utm_campaign"] || "",
+            utm_term: urlParams$1["utm_term"] || "",
+            groupid: urlParams$1["groupid"] || "",
+            campaignid: urlParams$1["campaignid"] || "",
+          },
+          referrer: document.referrer,
+          device: {
+            type: isMobile$1() ? "mobile" : "desktop",
+            page: window.location.pathname,
+          },
+          ...(window.version && {
+            ab_version: window.version,
+          }),
+        },
+      }),
+    });
+  }
+
+  function triggerConversion(
+    conversionId,
+    callback = undefined,
+    timeout = 1500,
+  ) {
+    if (urlParams["utm_campaign"]?.toLowerCase().includes("remarketing")) {
+      callback();
+      return false;
+    }
+    let timeoutId = undefined;
+    if (callback !== undefined && timeout !== undefined) {
+      timeoutId = setTimeout(callback, timeout);
+    }
+    gtag("event", "conversion", {
+      send_to: `AW-10840240452/${conversionId}`,
+      event_callback:
+        callback !== undefined &&
+        (() => {
+          if (timeoutId !== undefined) {
+            clearTimeout(timeoutId);
+          }
+          callback();
+        }),
+    });
+    console.debug(`triggered conversion ${conversionId}`);
+    return false;
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    for (const link of document.links) {
+      if (link.hostname.endsWith(window.location.hostname)) {
+        const url = new URL(link.href);
+        for (const param of [
+          "utm_source",
+          "utm_medium",
+          "utm_campaign",
+          "utm_term",
+          "utm_content",
+          "campaignid",
+          "groupid",
+        ]) {
+          if (urlParams[param]) {
+            url.searchParams.set(param, urlParams[param]);
+          }
+        }
+        link.href = url.href;
+      }
+
+      const conversionId = link.getAttribute("ga-conversion-id");
+      if (conversionId) {
+        link.addEventListener("click", (e) => {
+          e.preventDefault();
+          triggerConversion(conversionId, () => {
+            window.location = link.href;
+          });
+        });
+      }
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    // immediately trigger page load
+    triggerEvent({ type: "PAGE_LOAD" });
+
+    if (!visitorCreated) return;
+
+    // send VISITOR_CREATED event to MarketingWebsite dataset
+    triggerEvent({ type: "VISITOR_CREATED" });
+    // send VISITOR_CREATED event to AgentUI dataset
+    triggerAgentEvent({ type: "VISITOR_CREATED" });
+  });
+
+  function searchEventValue(element) {
+    const value = element?.getAttribute("clickevent_type");
+    return (
+      (value && {
+        type: value,
+        value: element?.getAttribute("clickevent_value"),
+      }) ||
+      (element?.parentElement && searchEventValue(element?.parentElement))
+    );
+  }
+
+  document.addEventListener("click", (event) => {
+    // new way
+    const target = event.target.closest("[data-click-type]");
+    if (target) {
+      triggerEvent({
+        type: target.dataset.clickType,
+        value: target.dataset.clickValue,
+      });
+      return;
+    }
+    // old way
+    const value = searchEventValue(event.target);
+    if (value) {
+      triggerEvent(value);
+    }
+  });
+
   /**
    * @typedef {(step: HTMLElement) => void} StepSetupFunction
    * @typedef {(step: HTMLElement) => boolean} StepValidateFunction
@@ -920,5 +1172,14 @@
     });
     document.addEventListener("DOMContentLoaded", () => observer.disconnect());
   })();
+
+  window.triggetAgentEvent = triggerAgentEvent;
+  window.triggerEvent = triggerEvent;
+  window.triggerConversion = triggerConversion;
+  window.isMobile = isMobile$1;
+  window.urlParams = urlParams$1;
+  window.getCookie = getCookie$1;
+  window.setCookie = setCookie;
+  window.getOrSetCookie = getOrSetCookie;
 
 })();
--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+import { triggerAgentEvent, triggerEvent } from "./analytics/api";
+import { triggerConversion } from "./ga";
+import { isMobile, urlParams } from "./util";
+import { getCookie, setCookie, getOrSetCookie } from "./cookies";
+import "./analytics/events";
 import "./forms/main";
+
+window.triggetAgentEvent = triggerAgentEvent;
+window.triggerEvent = triggerEvent;
+window.triggerConversion = triggerConversion;
+window.isMobile = isMobile;
+window.urlParams = urlParams;
+window.getCookie = getCookie;
+window.setCookie = setCookie;
+window.getOrSetCookie = getOrSetCookie;
