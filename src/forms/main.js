3516d6ce854fb9473eba11bdb571f586f8ab7801
Michael Gur <michael@thegur.net>
2025-06-16 17:33:15 +0300

    fix selector

--- src/forms/main.js
+++ src/forms/main.js
@@ -40,7 +40,7 @@ window.forms = {
 
 (() => {
   const observer = new MutationObserver(() => {
-    const el = document.querySelector("[data-form]");
+    const el = document.querySelector("form[data-form]");
     if (el) {
       observer.disconnect();
       window.forms.flow = multiStepFormInit({ form: el });
